d5332561b2fad5ba85fcbac47cd9c9a2dc53e6ba
liu1451182540 <liuguoxin0919@foxmail.com>
2015-11-05 20:07:15 +0800

    first commit

---
+++ Assets/Scripts/ImageButtonScr.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImageButtonScr : MonoBehaviour {
+	protected bool flag = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnClick () {
+		if (this.flag) {
+			this.GetComponent<UISprite>().spriteName = "checkButton_2";
+			this.GetComponent<UIButton>().normalSprite = "checkButton_2";
+			this.flag = false;
+		} else {
+			this.GetComponent<UISprite>().spriteName = "checkButton_1";
+			this.GetComponent<UIButton>().normalSprite = "checkButton_1";
+			this.flag = true;
+		}
+	}
+
+}
---
+++ Assets/Scripts/SendBtnScr.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendBtnScr : MonoBehaviour {
+	public UITextList textList;
+
+	static int count = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			this.OnClick();
+		}
+	}
+
+	void OnClick () {
+		GameObject input_Label = GameObject.Find ("Input_Label");
+		string text_str = "[8bddfc]Some say:[-] " +"[33ff22]"+ count + input_Label.GetComponent<UILabel> ().text;
+		textList.Add (text_str);
+		count ++;
+	}
+}
